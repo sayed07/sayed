51f04889a6e1659ed1429719e64e60d7d13feafc
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-07-01 10:54:26 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+character=input()
+vowels=['a','e','i','o','u']
+consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if character in vowels or character in consonants:
+    print("Alphabet")
+else:
+    print("No")
