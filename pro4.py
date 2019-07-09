e77a04c7329976f76bd7c13d46e048fba75b15ff
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-07-09 15:40:12 +0530

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -1,11 +1,11 @@
-pp,qq=map(str,input().split())
-was=0
-if len(pp)>len(qq):
-  pp,qq=qq,dbj
+p,q=map(str,input().split())
+n=0
+if len(p)>len(q):
+  p,q=q,p
 i=0
-while n<len(vs):
-  was+=(ord(qq[n])-ord(pp[n]))
-  n+=1
-for r in range(r,len(qq)):
-  was+=ord(qq[r])-ord('a')+1
-print(was)
+while i<len(p):
+  n+=(ord(q[i])-ord(p[i]))
+  i+=1
+for i in range(i,len(q)):
+  n+=ord(q[i])-ord('a')+1
+print(n)
