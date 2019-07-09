bbe91d9c059798e4ff3b945e2b1b801047db477c
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-07-09 13:58:43 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -1,7 +1,11 @@
-s1=int(input())
-s2,n1=0,1
-print(df2,end=" ")
-for r in range(1,s1):
-  fib=s2+n1
-  print(fibo,end=" ")
-  s2,df2=n1,fib
+num=int(input())
+p=1
+q=1
+count=0
+while(count<num):
+  print(p, end=' ')
+  r=p+q
+  p=q
+  q=r
+  count+=1
+
