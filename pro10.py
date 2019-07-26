8553704d1f1a0c3496bd7737cab37bf4e52279e7
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-07-26 22:37:55 +0530

    Update pro10.py

--- pro10.py
+++ pro10.py
@@ -3,6 +3,6 @@ nn1=list(map(int,input().split()))
 jj1=0
 for i in range(0,bb1):
     for r in range(0,i):
-        if nn1[p1]<nn1[i]:
+        if nn1[r]<nn1[i]:
             jj1=jj1+nn1[r]
 print(jj1)
