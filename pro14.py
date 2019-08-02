5724cbfc72e8354b2a294707fd00ee16562d0499
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-08-02 05:02:04 -0400

    Update pro14.py

--- pro14.py
+++ pro14.py
@@ -1,10 +1,13 @@
-pp=input()
-qq=map(int,input().split())
-rr=[]
-for n in qq:
-    eat=bin(n)
-    rr.append(eat)
-fin=sorted(rr)
-fin.reverse()
-for r in fin:
-    print(int(r,2))
+pp,qq=map(int,input().split())
+lis=list(map(int,input().split()))
+pp=[]
+lis.insert(0,0)
+for r in range(qq):
+     vin=[]
+     j=0
+     aa,bb=map(int,input().split())
+     for i in range(aa,bb+1):         
+         j^=lis[i]     
+     pp.append(j)
+for r in pp:
+     print(r)
