9e6c8fa62aed101d9c656d79e8579594c14dd93a
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-08-05 05:47:46 -0400

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,12 @@
+nn = int(input())
+lis = list(map(int, input().split()))
+maximum = 0
+cont = 0
+for r in range(nn-1):
+    if lis[r] < lis[r+1]:
+        cnt +=1
+        if maximum < cont:
+            maximum = cont
+    else:
+        cont = 0
+print(maximum+1)
