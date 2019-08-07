ee811a6cc1ff707b96659e434bd99540b1beb2ca
sayed07 <52285626+sayed07@users.noreply.github.com>
2019-08-07 05:56:52 -0400

    Create pro29.py

---
+++ pro29.py
@@ -0,0 +1,29 @@
+inp=int(input())
+
+bb=0
+
+nn=0
+
+a=[]
+
+while bb<90 and bb<inp:
+
+  s=0
+
+  for j in str(inp-bb):
+
+    s+=int(j)
+
+  if s+(inp-ii)==inp:
+
+    nn+=1
+
+    a.append(inp-bb)
+
+  bb+=1
+
+print(nn)
+
+for bb in a:
+
+  print(bb)
